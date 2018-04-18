bb699f44807e0e575cc97e712706e796322a4f75
M-T-Asagi <t.asagi00+github@gmail.com>
2018-04-19 02:19:35 +0900

    Serialize処理を追加

--- AddUnityDll/Program.cs
+++ AddUnityDll/Program.cs
@@ -7,14 +7,52 @@ namespace AddUnityDll
     {
         static void Main(string[] args)
         {
+            if(args.Length < 2)
+            {
+                throw new Exception("Error! invalid options.");
+            }
+
             string csproj = args[0];
             string config = args[1];
 
             Root deserializedObject = CsprojSerializer.Deserialize<Root>(csproj);
             Dictionary<string, string> dlls = ConfigReader.Read(config);
 
+            List<Reference> references;
+            if (deserializedObject.ItemGroup == null)
+            {
+                ItemGroup itemGroup = new ItemGroup();
+                references = new List<Reference>();
+                itemGroup.References = references;
+                deserializedObject.ItemGroup = itemGroup;
+            } else
+            {
+                references = deserializedObject.ItemGroup.References;
+            }
 
+            foreach(KeyValuePair<string, string> dll in dlls)
+            {
+                Reference reference = new Reference();
+                reference.Include = dll.Key;
+                reference.HintPath = dll.Value;
 
+                bool updated = false;
+
+                for(int i = 0;i < references.Count; i++)
+                {
+                    Reference _ref = references[i];
+                    if(_ref.Include == reference.Include || _ref.HintPath == reference.HintPath)
+                    {
+                        references[i] = reference;
+                        updated = true;
+                    }
+                }
+
+                if (!updated)
+                    references.Add(reference);
+            }
+            
+            CsprojSerializer.Serialize(csproj, deserializedObject);
         }
     }
 }
