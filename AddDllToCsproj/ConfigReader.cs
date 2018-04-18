414d778c7fd533ec625ed9f78d8ee8d87c075289
M-T-Asagi <t.asagi00+github@gmail.com>
2018-04-19 02:41:28 +0900

    rename Project

--- AddDllToCsproj/ConfigReader.cs
+++ AddDllToCsproj/ConfigReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace AddUnityDll
+namespace AddDllToCsproj
 {
     public static class ConfigReader
     {
--- AddDllToCsproj/CsprojSerializer.cs
+++ AddDllToCsproj/CsprojSerializer.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 
-namespace AddUnityDll
+namespace AddDllToCsproj
 {
     public static class CsprojSerializer
     {
